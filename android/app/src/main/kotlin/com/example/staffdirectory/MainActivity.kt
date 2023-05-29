48876547b077ddec5e9643c371c934ad557cb397
Akilo Rohimat <akilorohimat67@gamil.com>
2023-05-29 17:51:33 +0100

    first commit - committing all files to the repository

---
+++ android/app/src/main/kotlin/com/example/staffdirectory/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.staffdirectory
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:staffdirectory/login.dart';
+
+void main() {
+  runApp(myApp(MaterialApp as Function(Key? key)));
+}
+
+class myApp extends StatelessWidget {  
+  // ignore: use_function_type_syntax_for_parameters
+  const myApp((Key? key)) : super(key: key);
+
+
+@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    ); 
+  }
+}
+ 
